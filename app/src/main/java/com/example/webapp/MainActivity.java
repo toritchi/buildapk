76af9d07eb630eaa4fd5a2d0bc716250981c78d3
toritchi <78111843+toritchi@users.noreply.github.com>
2026-05-25 18:35:35 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://cummins-service.onrender.com/");
+    }
+}
